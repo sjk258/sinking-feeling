8e1f34c5a769bdf0255ae2405aa48784feeb236f
Thomas Kent <teeks99@yahoo.com>
2016-06-11 22:12:43 -0500

    Test for actual code.
    I added a test that checks that the template on our home page renders a line
    correctly.

---
+++ client/main.test.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+import { Tracker } from 'meteor/tracker';
+
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import './main.js';
+
+describe('Main', () => {
+  it('check initial message', () => {
+    var expected = 'David suggests clicking the button.';
+    
+    var context = { counter: 0 };
+    var hello = Template['hello'];
+    
+    var el = document.createElement('div');
+    document.body.appendChild(el);
+    Blaze.renderWithData(hello, context, el);
+    Tracker.flush();
+    assert.equal(expected, el.lastElementChild.firstChild.nodeValue);
+    
+    document.body.removeChild(el);
+  });
+});
+
