43d7a05d58c8453b1970092f35ce3ef7d18c7bba
stephenbush <sbbush1@bellsouth.net>
2016-06-17 16:10:05 -0400

    local commit

---
+++ gamerules/ui/gameRules.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+
+import { Games } from '../api/games.js';
+
+import './game.js';
+import './body.html';
+
+Template.body.helpers({
+	games() {
+		return Games.find({});
+	},
+	testGame() {
+		return Games.find({});
+	}
+});
