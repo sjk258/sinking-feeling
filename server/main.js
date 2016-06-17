2ffe1d4f9dd21596349c47e31e5ec6f0424cd690
Steve Kusner <sjk258@gmail.com>
2016-06-16 22:05:20 -0400

    Removed test that no longer applies. Switched gameboard state to use arrays

--- server/main.js
+++ server/main.js
@@ -6,37 +6,45 @@ Meteor.startup(() => {
 	// code to run on server at startup
 	console.log("Starting up...");
 
-	if (Games.find().count() === 0)
+	if (Games.find( { _id:'test' } ).count() === 1)
 	{
-		Games.insert({
+		Games.remove( { _id:'test' } );
+		console.log("Removed test game...");
+	}
+
+	Games.insert({
 		  	_id: "test",
 			owner_id: "test",
 			state: "ACTIVE",
 			time_created: new Date(),
 			computer_id: "SUE",
 			owner_board: {
-				A0: "E", B0: "E", C0: "E", D0: "E", E0: "E", F0: "E", G0: "E", H0: "E", I0: "E", J0: "E", 
-				A1: "E", B1: "E", C1: "E", D1: "E", E1: "E", F1: "E", G1: "E", H1: "E", I1: "E", J1: "E", 
-				A2: "E", B2: "E", C2: "E", D2: "E", E2: "E", F2: "E", G2: "E", H2: "E", I2: "E", J2: "E", 
-				A3: "E", B3: "E", C3: "E", D3: "E", E3: "E", F3: "E", G3: "E", H3: "E", I3: "E", J3: "E", 
-				A4: "E", B4: "E", C4: "E", D4: "E", E4: "E", F4: "E", G4: "E", H4: "E", I4: "E", J4: "E", 
-				A5: "E", B5: "E", C5: "E", D5: "E", E5: "E", F5: "E", G5: "E", H5: "E", I5: "E", J5: "E", 
-				A6: "E", B6: "E", C6: "E", D6: "E", E6: "E", F6: "E", G6: "E", H6: "E", I6: "E", J6: "E", 
-				A7: "E", B7: "E", C7: "E", D7: "E", E7: "E", F7: "E", G7: "E", H7: "E", I7: "E", J7: "E", 
-				A8: "E", B8: "E", C8: "E", D8: "E", E8: "E", F8: "E", G8: "E", H8: "E", I8: "E", J8: "E", 
-				A9: "E", B9: "E", C9: "E", D9: "E", E9: "E", F9: "E", G9: "E", H9: "E", I9: "E", J9: "E"
+				row: [
+					{ col: [ { pos: "A0", val: "E" }, { pos: "A1", val: "E" }, { pos: "A2", val: "E" }, { pos: "A3", val: "E" }, { pos: "A4", val: "E" }, { pos: "A5", val: "E" }, { pos: "A6", val: "E" }, { pos: "A7", val: "E" }, { pos: "A8", val: "E" }, { pos: "A9", val: "E" } ] }, 
+					{ col: [ { pos: "B0", val: "E" }, { pos: "B1", val: "E" }, { pos: "B2", val: "E" }, { pos: "B3", val: "E" }, { pos: "B4", val: "E" }, { pos: "B5", val: "E" }, { pos: "B6", val: "E" }, { pos: "B7", val: "E" }, { pos: "B8", val: "E" }, { pos: "B9", val: "E" } ] }, 
+					{ col: [ { pos: "C0", val: "E" }, { pos: "C1", val: "E" }, { pos: "C2", val: "E" }, { pos: "C3", val: "E" }, { pos: "C4", val: "E" }, { pos: "C5", val: "E" }, { pos: "C6", val: "E" }, { pos: "C7", val: "E" }, { pos: "C8", val: "E" }, { pos: "C9", val: "E" } ] }, 
+					{ col: [ { pos: "D0", val: "E" }, { pos: "D1", val: "E" }, { pos: "D2", val: "E" }, { pos: "D3", val: "E" }, { pos: "D4", val: "E" }, { pos: "D5", val: "E" }, { pos: "D6", val: "E" }, { pos: "D7", val: "E" }, { pos: "D8", val: "E" }, { pos: "D9", val: "E" } ] }, 
+					{ col: [ { pos: "E0", val: "E" }, { pos: "E1", val: "E" }, { pos: "E2", val: "E" }, { pos: "E3", val: "E" }, { pos: "E4", val: "E" }, { pos: "E5", val: "E" }, { pos: "E6", val: "E" }, { pos: "E7", val: "E" }, { pos: "E8", val: "E" }, { pos: "E9", val: "E" } ] }, 
+					{ col: [ { pos: "F0", val: "E" }, { pos: "F1", val: "E" }, { pos: "F2", val: "E" }, { pos: "F3", val: "E" }, { pos: "F4", val: "E" }, { pos: "F5", val: "E" }, { pos: "F6", val: "E" }, { pos: "F7", val: "E" }, { pos: "F8", val: "E" }, { pos: "F9", val: "E" } ] }, 
+					{ col: [ { pos: "G0", val: "E" }, { pos: "G1", val: "E" }, { pos: "G2", val: "E" }, { pos: "G3", val: "E" }, { pos: "G4", val: "E" }, { pos: "G5", val: "E" }, { pos: "G6", val: "E" }, { pos: "G7", val: "E" }, { pos: "G8", val: "E" }, { pos: "G9", val: "E" } ] }, 
+					{ col: [ { pos: "H0", val: "E" }, { pos: "H1", val: "E" }, { pos: "H2", val: "E" }, { pos: "H3", val: "E" }, { pos: "H4", val: "E" }, { pos: "H5", val: "E" }, { pos: "H6", val: "E" }, { pos: "H7", val: "E" }, { pos: "H8", val: "E" }, { pos: "H9", val: "E" } ] }, 
+					{ col: [ { pos: "I0", val: "E" }, { pos: "I1", val: "E" }, { pos: "I2", val: "E" }, { pos: "I3", val: "E" }, { pos: "I4", val: "E" }, { pos: "I5", val: "E" }, { pos: "I6", val: "E" }, { pos: "I7", val: "E" }, { pos: "I8", val: "E" }, { pos: "I9", val: "E" } ] }, 
+					{ col: [ { pos: "J0", val: "E" }, { pos: "J1", val: "E" }, { pos: "J2", val: "E" }, { pos: "J3", val: "E" }, { pos: "J4", val: "E" }, { pos: "J5", val: "E" }, { pos: "J6", val: "E" }, { pos: "J7", val: "E" }, { pos: "J8", val: "E" }, { pos: "J9", val: "E" } ] }, 
+				]
 			},
 			opponent_board: {
-				A0: "E", B0: "E", C0: "E", D0: "E", E0: "E", F0: "E", G0: "E", H0: "E", I0: "E", J0: "E", 
-				A1: "E", B1: "E", C1: "E", D1: "E", E1: "E", F1: "E", G1: "E", H1: "E", I1: "E", J1: "E", 
-				A2: "E", B2: "E", C2: "E", D2: "E", E2: "E", F2: "E", G2: "E", H2: "E", I2: "E", J2: "E", 
-				A3: "E", B3: "E", C3: "E", D3: "E", E3: "E", F3: "E", G3: "E", H3: "E", I3: "E", J3: "E", 
-				A4: "E", B4: "E", C4: "E", D4: "E", E4: "E", F4: "E", G4: "E", H4: "E", I4: "E", J4: "E", 
-				A5: "E", B5: "E", C5: "E", D5: "E", E5: "E", F5: "E", G5: "E", H5: "E", I5: "E", J5: "E", 
-				A6: "E", B6: "E", C6: "E", D6: "E", E6: "E", F6: "E", G6: "E", H6: "E", I6: "E", J6: "E", 
-				A7: "E", B7: "E", C7: "E", D7: "E", E7: "E", F7: "E", G7: "E", H7: "E", I7: "E", J7: "E", 
-				A8: "E", B8: "E", C8: "E", D8: "E", E8: "E", F8: "E", G8: "E", H8: "E", I8: "E", J8: "E", 
-				A9: "E", B9: "E", C9: "E", D9: "E", E9: "E", F9: "E", G9: "E", H9: "E", I9: "E", J9: "E"
+				row: [
+					{ col: [ { pos: "A0", val: "E" }, { pos: "A1", val: "E" }, { pos: "A2", val: "E" }, { pos: "A3", val: "E" }, { pos: "A4", val: "E" }, { pos: "A5", val: "E" }, { pos: "A6", val: "E" }, { pos: "A7", val: "E" }, { pos: "A8", val: "E" }, { pos: "A9", val: "E" } ] }, 
+					{ col: [ { pos: "B0", val: "E" }, { pos: "B1", val: "E" }, { pos: "B2", val: "E" }, { pos: "B3", val: "E" }, { pos: "B4", val: "E" }, { pos: "B5", val: "E" }, { pos: "B6", val: "E" }, { pos: "B7", val: "E" }, { pos: "B8", val: "E" }, { pos: "B9", val: "E" } ] }, 
+					{ col: [ { pos: "C0", val: "E" }, { pos: "C1", val: "E" }, { pos: "C2", val: "E" }, { pos: "C3", val: "E" }, { pos: "C4", val: "E" }, { pos: "C5", val: "E" }, { pos: "C6", val: "E" }, { pos: "C7", val: "E" }, { pos: "C8", val: "E" }, { pos: "C9", val: "E" } ] }, 
+					{ col: [ { pos: "D0", val: "E" }, { pos: "D1", val: "E" }, { pos: "D2", val: "E" }, { pos: "D3", val: "E" }, { pos: "D4", val: "E" }, { pos: "D5", val: "E" }, { pos: "D6", val: "E" }, { pos: "D7", val: "E" }, { pos: "D8", val: "E" }, { pos: "D9", val: "E" } ] }, 
+					{ col: [ { pos: "E0", val: "E" }, { pos: "E1", val: "E" }, { pos: "E2", val: "E" }, { pos: "E3", val: "E" }, { pos: "E4", val: "E" }, { pos: "E5", val: "E" }, { pos: "E6", val: "E" }, { pos: "E7", val: "E" }, { pos: "E8", val: "E" }, { pos: "E9", val: "E" } ] }, 
+					{ col: [ { pos: "F0", val: "E" }, { pos: "F1", val: "E" }, { pos: "F2", val: "E" }, { pos: "F3", val: "E" }, { pos: "F4", val: "E" }, { pos: "F5", val: "E" }, { pos: "F6", val: "E" }, { pos: "F7", val: "E" }, { pos: "F8", val: "E" }, { pos: "F9", val: "E" } ] }, 
+					{ col: [ { pos: "G0", val: "E" }, { pos: "G1", val: "E" }, { pos: "G2", val: "E" }, { pos: "G3", val: "E" }, { pos: "G4", val: "E" }, { pos: "G5", val: "E" }, { pos: "G6", val: "E" }, { pos: "G7", val: "E" }, { pos: "G8", val: "E" }, { pos: "G9", val: "E" } ] }, 
+					{ col: [ { pos: "H0", val: "E" }, { pos: "H1", val: "E" }, { pos: "H2", val: "E" }, { pos: "H3", val: "E" }, { pos: "H4", val: "E" }, { pos: "H5", val: "E" }, { pos: "H6", val: "E" }, { pos: "H7", val: "E" }, { pos: "H8", val: "E" }, { pos: "H9", val: "E" } ] }, 
+					{ col: [ { pos: "I0", val: "E" }, { pos: "I1", val: "E" }, { pos: "I2", val: "E" }, { pos: "I3", val: "E" }, { pos: "I4", val: "E" }, { pos: "I5", val: "E" }, { pos: "I6", val: "E" }, { pos: "I7", val: "E" }, { pos: "I8", val: "E" }, { pos: "I9", val: "E" } ] }, 
+					{ col: [ { pos: "J0", val: "E" }, { pos: "J1", val: "E" }, { pos: "J2", val: "E" }, { pos: "J3", val: "E" }, { pos: "J4", val: "E" }, { pos: "J5", val: "E" }, { pos: "J6", val: "E" }, { pos: "J7", val: "E" }, { pos: "J8", val: "E" }, { pos: "J9", val: "E" } ] },
+				]
 			},
 			time_started: new Date(),
 			turn_number: 0,
@@ -48,6 +56,6 @@ Meteor.startup(() => {
 				{ owner: "Sinking Feeling", text: "Welcome to the game!" },
 			]
   		});
-	}
+
   	console.log("Completing startup...");
 });
