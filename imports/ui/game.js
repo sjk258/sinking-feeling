b20d2edfd30d7d23b0b73409bc757a3c84c3cbe9
David B. Nagle <nagledb@gmail.com>
2016-06-21 22:34:29 +0000

    Simplified convertToIndex.

--- imports/ui/game.js
+++ imports/ui/game.js
@@ -92,29 +92,5 @@ Template.board_cell.events({
 });
 
 function convertToIndex(val) {
-  switch(val)
-  {
-    case 'A':
-      return 0;
-    case 'B':
-      return 1;
-    case 'C':
-      return 2;
-    case 'D':
-      return 3;
-    case 'E':
-      return 4;
-    case 'F':
-      return 5;
-    case 'G':
-      return 6;
-    case 'H':
-      return 7;
-    case 'I':
-      return 8;
-    case 'J':
-      return 9;
-    default:
-      return -1;
-  }
+  return 'ABCDEFGHIJ'.indexOf(val);
 }
