3a3ce51a871121edc389a622116ef76a5edbc0d3
David B. Nagle <nagledb@gmail.com>
2016-07-01 07:33:14 +0000

    Added button to create a new game.

--- imports/ui/home.js
+++ imports/ui/home.js
@@ -1,6 +1,8 @@
+/* globals FlowRouter */
 import { Template } from 'meteor/templating';
 
 import { Games } from '../api/games.js';
+import * as Game from '../api/game.js';
 
 import './home.html';
 
@@ -8,4 +10,13 @@ Template.home.helpers({
   games() {
     return Games.find({});
   },
+});
+
+Template.home.events({
+  'click #createGame'(event) {
+    const game = Game.create('test');
+    game.current_player = "creator";
+    Games.update(game._id, {$set: game});
+    FlowRouter.go('game', { id: game._id });
+  }
 });
