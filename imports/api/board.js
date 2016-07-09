77fc0cddbbb563e3f16ede3e06787ef7b9d73d21
David B. Nagle <nagledb@gmail.com>
2016-07-09 03:43:16 +0000

    Refactored board functionality from api/game.js to api/board.js.

--- imports/api/board.js
+++ imports/api/board.js
@@ -32,3 +32,102 @@ export function setRange(board, row, col, rowCount, colCount, val) {
     }
   }
 }
+
+// only exported for testing, don't call this
+export function addShips(board, ships, mark) {
+  for(let j = 0; j < ship_types.length; j++) {
+    const ship = ship_types[j];
+    if(!(ship in ships)) continue;
+    let row = ships[ship].row;
+    let col = ships[ship].col;
+    for(let i = 0; i < ship_lengths[ship]; i++)
+    {
+      board[row][col].shipNum = j;
+
+      if(ships[ship].vertical)
+      {
+        if(mark && i === 0)
+        {
+          // Ship at top
+          board[row][col].val = 'S_Top';
+        }
+        else if(mark && i === (ship_lengths[ship] - 1))
+        {
+          // Ship at bottom
+          board[row][col].val = 'S_Bottom';
+        }
+        else if(mark)
+        {
+          // Mid-piece of vertical ship
+          board[row][col].val = 'S_Vertical';
+        }
+
+        row++;
+      }
+      else
+      {
+        if(mark && i === 0)
+        {
+          // Ship at top
+          board[row][col].val = 'S_Left';
+        }
+        else if(mark && i === (ship_lengths[ship] - 1))
+        {
+          // Ship at bottom
+          board[row][col].val = 'S_Right';
+        }
+        else if(mark)
+        {
+          // Mid-piece of vertical ship
+          board[row][col].val = 'S_Horizontal';
+        }
+
+        col++;
+      }
+    }
+  }
+}
+
+// only exported for testing, don't call this
+export function addShots(board, shots, ships) {
+  shots.forEach(function(shot){
+    if(spaceIsOnShip(shot, ships))
+    {
+      board[shot.row][shot.col].val = 'H';
+    }
+    else
+    {
+      board[shot.row][shot.col].val = 'M';
+    }
+  });
+}
+
+// only exported for testing, don't call this
+export function spaceIsOnShip(space, ships){
+  for(var ship in ships)
+  {
+    for(let i = 0; i < ship_lengths[ship]; i++) {
+      var ship_space = { row: ships[ship].row, col: ships[ship].col };
+      if(ships[ship].vertical){
+        ship_space.row += i;
+      }
+      else{
+        ship_space.col += i;
+      }
+      if(spacesAreSame(space, ship_space))
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// only exported for testing, don't call this
+export function spacesAreSame(space1, space2){
+  if((space1.row == space2.row) && (space1.col == space2.col))
+  {
+    return true;
+  }
+  return false;
+}
--- imports/api/board.test.js
+++ imports/api/board.test.js
@@ -8,7 +8,7 @@ export function checkBoard(expected, board)
     for(c = 0; c < 10; c++) {
       assert.equal(expected[r][c], board[r][c].val, "row: " + r.toString() + " col: " + c.toString());
     }
-  }  
+  }
 }
 
 describe('board', function() {
@@ -37,16 +37,15 @@ describe('board', function() {
       }
     });
   });
-  
-  
-  describe('setRange', function	     () {
+
+  describe('setRange', function() {
     function testBoard(expected, x, y, len, width, value)
     {
       const board = Board.makeEmptyBoard();
       Board.setRange(board, x, y, len, width, value);
       checkBoard(expected, board);
     }
-    
+
     it('works correctly for a single cell', function () {
       const exp = [
         "EEEEEEEEEE",
@@ -108,4 +107,88 @@ describe('board', function() {
       testBoard(exp, 6, 3, 2, 3, 'X');
     });
   });
+
+  describe('spacesAreSame', function() {
+    it('spaces are the same', function(){
+      var space1 = {row: 1, col: 2};
+      var space2 = {row: 1, col: 2};
+
+      assert.equal(true, Board.spacesAreSame(space1, space2));
+
+      space1.row = 3;
+      assert.equal(false, Board.spacesAreSame(space1, space2));
+
+      space1.row = 1;
+      space1.col = 3;
+      assert.equal(false, Board.spacesAreSame(space1, space2));
+
+      space1.row = 3;
+      space1.col = 3;
+      assert.equal(false, Board.spacesAreSame(space1, space2));
+
+      space1.row = 2;
+      space1.col = 1;
+      assert.equal(false, Board.spacesAreSame(space1, space2));
+    });
+  });
+  describe('spaceIsOnShip', function() {
+    it('shot to carrier vertical', function(){
+      var ships = { carrier: { row: 0, col: 0, vertical: true } };
+      var shot = { row: 0, col: 0 };
+
+      assert.equal(true, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 1;
+      assert.equal(true, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 4;
+      assert.equal(true, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 5;
+      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 0;
+      shot.col = 1;
+      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+    });
+    it('shot to destroyer horizontal', function(){
+      var ships = { destroyer: { row: 0, col: 0, vertical: false } };
+      var shot = { row: 0, col: 0 };
+
+      assert.equal(true, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 1;
+      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+
+      shot.row = 0;
+      shot.col = 1;
+      assert.equal(true, Board.spaceIsOnShip(shot, ships));
+
+      shot.col = 2;
+      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+    });
+    it('multiple ships', function(){
+      var ships = {
+        carrier: { row: 0, col: 0, vertical: true },
+        cruiser: { row: 0, col: 1, vertical: true },
+        destroyer: { row: 0, col: 2, vertical: false }
+      };
+      var success_shots = [
+        {row: 0, col: 0}, {row: 1, col: 0}, {row: 4, col: 0}, {row: 0, col: 1},
+        {row: 2, col: 1}, {row: 0, col: 2}, {row: 0, col: 3}
+      ];
+      var fail_shots = [
+        {row: 5, col: 0}, {row: 3, col: 1}, {row: 1, col: 2}, {row: 1, col: 3},
+        {row: 0, col: 4}
+      ];
+
+      success_shots.forEach( function(shot){
+        assert.equal(true, Board.spaceIsOnShip(shot, ships));
+      });
+      fail_shots.forEach( function(shot){
+        assert.equal(false, Board.spaceIsOnShip(shot, ships));
+      });
+    });
+  });
+
 });
