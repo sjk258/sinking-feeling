7278b69ebae962839f4735cea052acd26f01f777
David B. Nagle <nagledb@gmail.com>
2016-07-09 04:05:55 +0000

    Refactored board test cases.
    Isolated individual test cases. Made style consistent through file.

--- imports/api/board.test.js
+++ imports/api/board.test.js
@@ -1,8 +1,7 @@
 import { assert } from 'meteor/practicalmeteor:chai';
 import * as Board from './board.js';
 
-export function checkBoard(expected, board)
-{
+export function checkBoard(expected, board) {
   let r, c;
   for(r = 0; r < 10; r++) {
     for(c = 0; c < 10; c++) {
@@ -39,14 +38,13 @@ describe('board', function() {
   });
 
   describe('setRange', function() {
-    function testBoard(expected, x, y, len, width, value)
-    {
+    function testBoard(expected, x, y, len, width, value) {
       const board = Board.makeEmptyBoard();
       Board.setRange(board, x, y, len, width, value);
       checkBoard(expected, board);
     }
 
-    it('works correctly for a single cell', function () {
+    it('works correctly for a single cell', function() {
       const exp = [
         "EEEEEEEEEE",
         "EEEEEEEEEE",
@@ -61,7 +59,7 @@ describe('board', function() {
       ];
       testBoard(exp, 2, 4, 1, 1, 'X');
     });
-    it('works correctly for a horizontal ship', function () {
+    it('works correctly for a horizontal ship', function() {
       const exp = [
         "EEEEEEEEEE",
         "EEEEEEEEEE",
@@ -76,7 +74,7 @@ describe('board', function() {
       ];
       testBoard(exp, 3, 1, 1, 4, 'X');
     });
-    it('works correctly for a vertical ship', function () {
+    it('works correctly for a vertical ship', function() {
       const exp = [
         "XEEEEEEEEE",
         "XEEEEEEEEE",
@@ -91,7 +89,7 @@ describe('board', function() {
       ];
       testBoard(exp, 0, 0, 5, 1, 'X');
     });
-    it('works correctly for a block', function () {
+    it('works correctly for a block', function() {
       const exp = [
         "EEEEEEEEEE",
         "EEEEEEEEEE",
@@ -109,84 +107,105 @@ describe('board', function() {
   });
 
   describe('spacesAreSame', function() {
-    it('spaces are the same', function(){
-      var space1 = {row: 1, col: 2};
-      var space2 = {row: 1, col: 2};
-
+    it('returns true when row and col are same', function() {
+      const space1 = {row: 1, col: 2};
+      const space2 = {row: 1, col: 2};
       assert.equal(true, Board.spacesAreSame(space1, space2));
-
-      space1.row = 3;
+    });
+    it('returns false when cols are same but rows differ', function() {
+      const space1 = {row: 3, col: 2};
+      const space2 = {row: 1, col: 2};
       assert.equal(false, Board.spacesAreSame(space1, space2));
-
-      space1.row = 1;
-      space1.col = 3;
+    });
+    it('returns false when rows are same but cols differ', function() {
+      const space1 = {row: 1, col: 3};
+      const space2 = {row: 1, col: 2};
       assert.equal(false, Board.spacesAreSame(space1, space2));
-
-      space1.row = 3;
-      space1.col = 3;
+    });
+    it('returns false when rows and cols both differ', function() {
+      const space1 = {row: 3, col: 3};
+      const space2 = {row: 1, col: 2};
       assert.equal(false, Board.spacesAreSame(space1, space2));
-
-      space1.row = 2;
-      space1.col = 1;
+    });
+    it('returns false when row and col are swapped', function() {
+      const space1 = {row: 2, col: 1};
+      const space2 = {row: 1, col: 2};
       assert.equal(false, Board.spacesAreSame(space1, space2));
     });
   });
+
   describe('spaceIsOnShip', function() {
-    it('shot to carrier vertical', function(){
-      var ships = { carrier: { row: 0, col: 0, vertical: true } };
-      var shot = { row: 0, col: 0 };
+    let ships;
+    describe('with vertical carrier', function() {
+      beforeEach(function() {
+        ships = { carrier: { row: 0, col: 0, vertical: true } };
+      });
 
-      assert.equal(true, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 1;
-      assert.equal(true, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 4;
-      assert.equal(true, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 5;
-      assert.equal(false, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 0;
-      shot.col = 1;
-      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+      it('returns true with hit on anchor square', function() {
+        const shot = { row: 0, col: 0 };
+        assert(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns true with hit on non-anchor square', function() {
+        const shot = { row: 4, col: 0 };
+        assert(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns false with miss on anchor col', function() {
+        const shot = { row: 5, col: 0 };
+        assert.isFalse(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns false with miss on anchor row', function() {
+        const shot = { row: 0, col: 1 };
+        assert.isFalse(Board.spaceIsOnShip(shot, ships));
+      });
     });
-    it('shot to destroyer horizontal', function(){
-      var ships = { destroyer: { row: 0, col: 0, vertical: false } };
-      var shot = { row: 0, col: 0 };
+    describe('with horizontal destroyer', function() {
+      beforeEach(function() {
+        ships = { destroyer: { row: 0, col: 0, vertical: false } };
+      });
 
-      assert.equal(true, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 1;
-      assert.equal(false, Board.spaceIsOnShip(shot, ships));
-
-      shot.row = 0;
-      shot.col = 1;
-      assert.equal(true, Board.spaceIsOnShip(shot, ships));
-
-      shot.col = 2;
-      assert.equal(false, Board.spaceIsOnShip(shot, ships));
+      it('returns true with hit on anchor square', function() {
+        const shot = { row: 0, col: 0 };
+        assert(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns true with hit on non-anchor square', function() {
+        const shot = { row: 0, col: 1 };
+        assert(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns false with miss on anchor col', function() {
+        const shot = { row: 1, col: 0 };
+        assert.isFalse(Board.spaceIsOnShip(shot, ships));
+      });
+      it('returns false with miss on anchor row', function() {
+        const shot = { row: 0, col: 2 };
+        assert.isFalse(Board.spaceIsOnShip(shot, ships));
+      });
     });
-    it('multiple ships', function(){
-      var ships = {
-        carrier: { row: 0, col: 0, vertical: true },
-        cruiser: { row: 0, col: 1, vertical: true },
-        destroyer: { row: 0, col: 2, vertical: false }
-      };
-      var success_shots = [
+    describe('with multiple ships', function() {
+      beforeEach(function() {
+        ships = {
+          carrier: { row: 0, col: 0, vertical: true },
+          cruiser: { row: 0, col: 1, vertical: true },
+          destroyer: { row: 0, col: 2, vertical: false }
+        };
+      });
+      const success_shots = [
         {row: 0, col: 0}, {row: 1, col: 0}, {row: 4, col: 0}, {row: 0, col: 1},
         {row: 2, col: 1}, {row: 0, col: 2}, {row: 0, col: 3}
       ];
-      var fail_shots = [
+      const fail_shots = [
         {row: 5, col: 0}, {row: 3, col: 1}, {row: 1, col: 2}, {row: 1, col: 3},
         {row: 0, col: 4}
       ];
 
-      success_shots.forEach( function(shot){
-        assert.equal(true, Board.spaceIsOnShip(shot, ships));
+      success_shots.forEach(function(shot) {
+        it('returns true with hit on ('+shot.row+','+shot.col+')', function() {
+          assert(Board.spaceIsOnShip(shot, ships));
+        });
       });
-      fail_shots.forEach( function(shot){
-        assert.equal(false, Board.spaceIsOnShip(shot, ships));
+      fail_shots.forEach(function(shot) {
+        it('returns false with miss on ('+shot.row+','+shot.col+')', function() {
+          assert.isFalse(Board.spaceIsOnShip(shot, ships));
+        });
       });
     });
   });
