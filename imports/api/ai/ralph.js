85ac97cdb1dfe28e65c57e4fa90d4c3a007de4ad
David B. Nagle <nagledb@gmail.com>
2016-07-15 00:43:58 +0000

    Restructured location of valid move in Ralph AI.

--- imports/api/ai/ralph.js
+++ imports/api/ai/ralph.js
@@ -10,16 +10,22 @@ export function intToMove(val) {
   return {row: Math.floor(val/10), col: (val%10)};
 }
 
+/* Returns the first valid move, or undefined if none found */
+export function firstValidMove(board, moves) {
+  const move = _.find(moves, (move) => {
+    return board[move.row][move.col].state === "E";
+  });
+  if(move === undefined) {
+    throw new Meteor.Error('no-moves-left', 'No more moves are possible');
+  }
+  return move;
+}
+
 /* jshint -W098 */
 // Disable reporting of unused variables, since we need to accept state but it
 // is unused here.
 export function makeMove(board, state) {
 /* jshint +W098 */
   let moves = _.shuffle(_.map(_.range(0, 100), intToMove));
-
-  for(let move of moves) {
-    if (board[move.row][move.col].state === "E") return move;
-  }
-
-  throw new Meteor.Error('no-moves-left', 'No more moves are possible');
+  return firstValidMove(board, moves);
 }
