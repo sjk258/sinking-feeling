40dcd68eb43c224a835e95d4401bbc2c434bc1fe
David B. Nagle <nagledb@gmail.com>
2016-07-15 02:44:49 +0000

    Added more tests for Ralph AI.

--- imports/api/ai/ralph.test.js
+++ imports/api/ai/ralph.test.js
@@ -1,4 +1,5 @@
 import { assert } from 'meteor/practicalmeteor:chai';
+import { _ } from 'meteor/underscore';
 import * as AI from './ralph.js';
 
 describe('api/ai/ralph.js', function() {
@@ -18,6 +19,67 @@ describe('api/ai/ralph.js', function() {
     it('should map 99 to {row: 9, col: 9}', function() {
       const move = AI.intToMove(1);
       assert.deepEqual(move, {row: 0, col: 1});
+    });
+  });
+
+  describe('shuffleIntToMoves', function() {
+    it('should return an array of the right length', function() {
+      const moves = AI.shuffleIntToMoves([0, 42, 99]);
+      assert.isArray(moves);
+      assert.lengthOf(moves, 3);
+    });
+    it('should return an array of objects', function() {
+      const moves = AI.shuffleIntToMoves([0, 42, 99]);
+      for(let i = 0; i < 3; i++) {
+        assert.isObject(moves[i]);
+        assert.property(moves[i], 'row');
+        assert.property(moves[i], 'col');
+      }
+    });
+    it('should return an array of objects with row and col props', function() {
+      const moves = AI.shuffleIntToMoves([0, 42, 99]);
+      for(let i = 0; i < 3; i++) {
+        assert.property(moves[i], 'row');
+        assert.property(moves[i], 'col');
+      }
+    });
+    it('should correctly convert ints to moves', function() {
+      const moves = AI.shuffleIntToMoves([0, 42, 99]);
+      const exp = [
+        {row: 0, col: 0},
+        {row: 4, col: 2},
+        {row: 9, col: 9},
+      ];
+      assert.sameDeepMembers(moves, exp);
+    });
+    it('should shuffle the list', function() {
+      // Construct 10 lists of 0 to 100
+      const listsOfVals = _.times(10, () => _.range(0, 100));
+      // Convert those lists into 10 lists of moves
+      const listsOfMoves = _.map(listsOfVals, AI.shuffleIntToMoves);
+      // Convert those move lists back into (presumably randomly shuffled)
+      // integer lists
+      const revertedVals = _.map(listsOfMoves, (move) => (move.row * 10 + move.col));
+      // This "rotates" the array so that instead of a list of 10 lists of 100
+      // integers, it's now 100 lists of 10 integers. So rotated[0] contains
+      // revertedVals[0][0], revertedVals[1][0], revertedVals[2][0], etc.
+      // In other words, rotated[i] contains the ith value from each subarray in
+      // revertedVals
+      const rotated = _.zip.apply(_, revertedVals);
+      let succeed = true;
+      for(let i = 0; i < 100; i++) {
+        // If there's only 1 unique value from the ith values, then they were
+        // all the same. We need to encounter at least one instance of them not
+        // being all the same to demonstrate that there's at least some measure
+        // of shuffling happening.
+        if(_.uniq(rotated[i]).length > 1) {
+          succeed = false;
+          break;
+        }
+      }
+      // If we reach here, we failed to find uniqueness which means no random
+      // shuffling occurred. This means this test fails.
+      assert(succeed);
     });
   });
 
