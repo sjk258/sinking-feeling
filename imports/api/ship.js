b3c0de1d092b1cb301d56ca2a86940d2d8d9beac
David B. Nagle <nagledb@gmail.com>
2016-07-17 08:10:36 +0000

    Code reformatting.

--- imports/api/ship.js
+++ imports/api/ship.js
@@ -1,2 +1,8 @@
-export const types = ["carrier", "battleship", "cruiser", "submarine", "destroyer"];
-export const lengths = { carrier: 5, battleship: 4, cruiser: 3, submarine: 3, destroyer: 2 };
+export const lengths = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 2,
+};
+export const types = Object.keys(lengths);
