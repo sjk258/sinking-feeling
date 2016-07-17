ad329005cae08073c4e1d64727b4911cca9e57af
David B. Nagle <nagledb@gmail.com>
2016-07-17 08:04:04 +0000

    Minor code tweaks.

--- imports/api/square.js
+++ imports/api/square.js
@@ -6,17 +6,17 @@ import * as Ship from './ship.js';
 
 // only exported for testing, don't call this
 export function spaceIsOnShip(space, ships) {
-  for(var ship in ships) {
-    if(spaceIsOnAShip(space, ships[ship], Ship.lengths[ship])) {
+  for(let type in ships) {
+    if(spaceIsOnAShip(space, ships[type], Ship.lengths[type])) {
       return true;
     }
   }
   return false;
 }
 
-export function spaceIsOnAShip(space, ship, length){
+export function spaceIsOnAShip(space, ship, length) {
   for(let i = 0; i < length; i++) {
-    var ship_space = { row: ship.row, col: ship.col };
+    const ship_space = { row: ship.row, col: ship.col };
     if(ship.vertical) {
       ship_space.row += i;
     } else {
@@ -30,11 +30,8 @@ export function spaceIsOnAShip(space, ship, length){
 }
 
 // only exported for testing, don't call this
-export function spacesAreSame(space1, space2){
-  if((space1.row == space2.row) && (space1.col == space2.col)) {
-    return true;
-  }
-  return false;
+export function spacesAreSame(space1, space2) {
+  return (space1.row === space2.row) && (space1.col === space2.col);
 }
 
 export function squareObjToName(obj) {
