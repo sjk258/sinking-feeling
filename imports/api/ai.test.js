d5bb54675694b18841d9db236ee39b51c9641ab3
David B. Nagle <nagledb@gmail.com>
2016-07-18 02:52:58 +0000

    Updated ai test suite to use correct BDD structure.

--- imports/api/ai.test.js
+++ imports/api/ai.test.js
@@ -5,49 +5,51 @@ import * as Board from './board.js';
 import * as AI from './ai.js';
 
 describe('api/ai.js', function() {
-  describe('method getNames', function() {
-    it('returns an array', function() {
+  describe('getNames', function() {
+    it('should return an array', function() {
       const names = AI.getNames();
       assert.isArray(names);
     });
-    it('contains sue', function() {
+    it('should contain sue', function() {
       const names = AI.getNames();
       assert.include(names, 'sue');
     });
   });
-  describe('attribute default_name', function() {
-    it('is a string', function() {
+
+  describe('default_name', function() {
+    it('should be a string', function() {
       assert.isString(AI.default_name);
     });
   });
-  describe('method getPlayer', function() {
+
+  describe('getPlayer', function() {
     const names = AI.getNames();
     names.push('invalid-name');
     names.forEach(function(name) {
-      describe('with name = ' + name, function() {
-        it('returns an object', function() {
+      context('with name = ' + name, function() {
+        it('should return an object', function() {
           const ai = AI.getPlayer(name);
           assert.isObject(ai);
         });
-        it('returns an object with string attribute name', function() {
+        it('should return an object with string attribute name', function() {
           const ai = AI.getPlayer(name);
           assert.isObject(ai);
           assert.property(ai, 'name');
           assert.isString(ai.name);
         });
-        it('returns an object with string attribute full_name', function() {
+        it('should return an object with string attribute full_name', function() {
           const ai = AI.getPlayer(name);
           assert.isObject(ai);
           assert.property(ai, 'full_name');
           assert.isString(ai.full_name);
         });
-        it('returns an object with method makeMove', function() {
+        it('should return an object with method makeMove', function() {
           const ai = AI.getPlayer(name);
           assert.isObject(ai);
           assert.property(ai, 'makeMove');
           assert.isFunction(ai.makeMove);
         });
-        it('makeMove returns a valid move on an empty board', function() {
+        it('should return a valid move when makeMove called on empty board', function() {
           const ai = AI.getPlayer(name);
           const board = {
             squares: Board.makeEmptyBoard(),
@@ -61,7 +63,7 @@ describe('api/ai.js', function() {
           assert.isNumber(move.row);
           assert.isNumber(move.col);
         });
-        it('makeMove throws error no-moves-left after 100 moves', function() {
+        it('should throw error no-moves-left when makeMove called 101st time', function() {
           const ai = AI.getPlayer(name);
           const board = {
             squares: Board.makeEmptyBoard(),
@@ -78,7 +80,7 @@ describe('api/ai.js', function() {
           assert.throws(makeMove, Meteor.Error, 'no-moves-left');
         });
         if(name != 'invalid-name') {
-          it('treats name without case sensitivity', function() {
+          it('should treat name without case sensitivity', function() {
             // Create a copy of the name where each character has alternating
             // case
             const garbled = _.map(name.split(''), function(c,i) {
