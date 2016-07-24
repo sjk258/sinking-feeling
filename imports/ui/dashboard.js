7403547b427f0a312b93f767d8e97ad9fc150e04
David B. Nagle <nagledb@gmail.com>
2016-07-24 11:51:07 +0000

    Split up dashboard into sections.

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -7,23 +7,101 @@ import './dashboard.less';
 import './listing.js';
 
 Template.dashboard.helpers({
-  games() {
-    if (Meteor.userId() !== null) {
-      return Games.find({
-        $or: [
-          {
-            'creator.id': Meteor.userId(),
-            'creator.remove': { $exists: false },
-          },
-          {
-            'challenger.id': Meteor.userId(),
-            'challenger.remove': { $exists: false },
-          },
-        ]
-      });
-    } else {
-      return [];
-    }
+  invited() {
+    const id = Meteor.userId();
+    if(!id) return [];
+    return Games.find({
+      state: 'pending',
+      'challenger.id': Meteor.userId(),
+      'challenger.remove': { $exists: false },
+    });
+  },
+  active() {
+    const id = Meteor.userId();
+    if(!id) return [];
+    return Games.find({
+      $or: [
+        {
+          'state': 'setup',
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+          'creator.ready': { $exists: false },
+        },
+        {
+          'state': 'setup',
+          'challenger.id': id,
+          'challenger.remove': { $exists: false },
+          'challenger.ready': { $exists: false },
+        },
+        {
+          'state': 'active',
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+          'current_player': 'creator',
+        },
+        {
+          'state': 'active',
+          'challenger.id': id,
+          'challenger.remove': { $exists: false },
+          'current_player': 'challenger',
+        },
+      ],
+    });
+  },
+  waiting() {
+    const id = Meteor.userId();
+    if(!id) return [];
+    return Games.find({
+      $or: [
+        {
+          'state': 'setup',
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+          'creator.ready': { $exists: true },
+        },
+        {
+          'state': 'setup',
+          'challenger.id': id,
+          'challenger.remove': { $exists: false },
+          'challenger.ready': { $exists: true },
+        },
+        {
+          'state': 'active',
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+          'current_player': 'challenger',
+        },
+        {
+          'state': 'active',
+          'challenger.id': id,
+          'challenger.remove': { $exists: false },
+          'current_player': 'creator',
+        },
+        {
+          'state': { $in: ['waiting', 'pending'] },
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+        }
+      ],
+    });
+  },
+  done() {
+    const id = Meteor.userId();
+    if(!id) return [];
+    return Games.find({
+      $or: [
+        {
+          'state': { $in: ['declined', 'ended'] },
+          'creator.id': id,
+          'creator.remove': { $exists: false },
+        },
+        {
+          'state': 'ended',
+          'challenger.id': id,
+          'challenger.remove': { $exists: false },
+        },
+      ],
+    });
   },
   title(game) {
     return Game.getTitle(game);
@@ -54,3 +132,52 @@ Template.dashboard.helpers({
     /* jshint +W086 */
   },
 });
+
+Template.dash_invited.helpers({
+  title(game) {
+    return Game.getTitle(game);
+  },
+  players(game) {
+    return "Invited by " + game.creator.name;
+  },
+});
+
+Template.dash_active.helpers({
+  title(game) {
+    return Game.getTitle(game);
+  },
+  players(game) {
+    return game.creator.name + " vs. " + game.challenger.name;
+  },
+});
+
+Template.dash_waiting.helpers({
+  title(game) {
+    return Game.getTitle(game);
+  },
+  players(game) {
+    switch(game.state) {
+      case 'waiting':
+        return "Waiting on player to join";
+      case 'pending':
+        return "You are waiting on " + game.challenger.name;
+      default:
+        return game.creator.name + " vs. " + game.challenger.name;
+    }
+  },
+});
+
+Template.dash_done.helpers({
+  title(game) {
+    return Game.getTitle(game);
+  },
+  players(game) {
+    switch(game.state) {
+      case 'declined':
+        return game.challenger.name + " declined your invitation";
+      default:
+        return game.creator.name + " vs. " + game.challenger.name;
+    }
+  },
+});
+
